41565c7a2ddaccd2228138f33caa8054736a04b5
Aynur Gallyamov <ainyr60@gmail.com>
2022-10-23 16:59:20 +0500

    2

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -10,6 +10,18 @@ class Sidebar {
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
+  /**
+ * Класс Sidebar отвечает за работу боковой колонки:
+ * кнопки скрытия/показа колонки в мобильной версии сайта
+ * и за кнопки меню
+ * */
+class Sidebar {
+  /**
+   * Запускает initAuthLinks и initToggleButton
+   * */
+  static init() {
+    this.initAuthLinks();
+    this.initToggleButton();
   }
 
   /**
@@ -18,7 +30,14 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
-
+    document.querySelector('.sidebar-toggle').onclick = (e) => {
+      e.preventDefault();
+      const body = document.querySelector('.sidebar-mini');
+      if (body.classList.contains('sidebar-open')) {
+        
+        body.classList.remove('sidebar-open', 'sidebar-collapse');
+      } else body.classList.add('sidebar-open', 'sidebar-collapse');
+    } 
   }
 
   /**
@@ -28,7 +47,25 @@ class Sidebar {
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
+  /* Обработка кнопки Вход*/
   static initAuthLinks() {
-
+    document.querySelector('.menu-item_login > a').onclick = e => {
+      App.getModal('login').open();
+      return false;
+    }
+    /* Обработка кнопки Регистрация*/
+    document.querySelector('.menu-item_register > a').onclick = e => {
+      e.preventDefault();
+      App.getModal('register').open();
+    }
+    /* Обработка кнопки Выход*/
+    document.querySelector('.menu-item_logout > a').onclick = e => {
+      e.preventDefault();
+      User.logout((err, resp) => {
+        if (resp && resp.success) {
+          App.setState('init');
+        }
+      })
+    }
   }
 }
